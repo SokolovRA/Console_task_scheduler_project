473f9f67f5f5bb7db9726141e1cc739adbe02f53
zero77787 <rsokolov995@gmail.com>
2023-01-24 19:55:09 +0300

    Курсовая работа №2(готовая)11

--- src/Main.java
+++ src/Main.java
@@ -17,7 +17,7 @@ public class Main {
         DailyTask dailyTask = new DailyTask("Для отдела", "Подготовить графики", Type.WORK, LocalDateTime.of(2022,2,2,15,15,00,00));
         WeeklyTask weeklyTask = new WeeklyTask("Директору","Передать отчет",Type.WORK,LocalDateTime.of(2022,2,2,15,15,00,00));
         MonthlyTask monthlyTask = new MonthlyTask("Draiv", "Jamp Pamp", Type.PERSONAL, LocalDateTime.of(2022, 2, 2, 22, 15, 00, 00));
-        YearlyTask yearlyTask = new YearlyTask("Поздравление с НГ ","Поздравить бабушку",Type.PERSONAL,LocalDateTime.of(2023,1,1,00,00,00,00));
+        YearlyTask yearlyTask = new YearlyTask("Поздравление с НГ   ","Поздравить бабушку",Type.PERSONAL,LocalDateTime.of(2023,1,1,00,00,00,00));
 
         TaskServis taskServis = new TaskServis();
         taskServis.add(oneTimeTask);
